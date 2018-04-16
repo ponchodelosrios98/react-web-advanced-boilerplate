923222c57a8bd89e3f880ec24bd3d42d9bad0cee
alfonsodelosrios98 <ponchodelosrios98@gmail.com>
2018-04-15 17:07:47 -0700

    Advanced boilerplate + SCSS

--- src/components/views/Home/HomeView.jsx
+++ src/components/views/Home/HomeView.jsx
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
 
 import './style.css';
-import ColorCard from '../../core/ColorCard';
 
-import thumbImage from '../../../img/home_image.png';
-import CampaignCard from '../../campaigns/CampaignCard';
-
-class Plans extends Component {
+class HomeView extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -20,29 +15,10 @@ class Plans extends Component {
   render() {
     return (
       <div className="view-container container home-view-container">
-        <Row className="header">
-          <Col sm={24} md={15} className="text-div" >
-            <p className="important">Hoy es un buen día para</p>
-            <h1>salvar vidas.</h1>
-            <p>
-              Blooders es la primera plataforma para mejorar
-              la experiencia de donar sangre y conectar a personas
-              que necesitan sangre con donantes y hospitales.
-            </p>
-          </Col>
-          <Col xs={24} sm={9} justify="center">
-            <ColorCard>
-              <img className="thumb" src={thumbImage} alt="Thumb" />
-              <p className="hashtag">#SomosBlooders</p>
-            </ColorCard>
-          </Col>
-        </Row>
-        <Row className="section">
-          <CampaignCard />
-        </Row>
+        <p>Home Page</p>
       </div>
     );
   }
 }
 
-export default Plans;
+export default HomeView;
